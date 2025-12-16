89b38a73df81cf0b85cd06ce9905f77788fb632b
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-12-16 21:28:23 +0100

    checkout function induced

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
 const parentOfPrice = document.querySelector(".price-container");
-const quantity = document.getElementById("quantity")
+const quantity = document.getElementById("quantity");
+const button = document.getElementById("button");
+const discountedPrice = document.getElementById("discount");
+const checkout = document.getElementById("product-item-container")
+const unitPrice = (Number(discountedPrice.innerText));
 
 let individualUnit = 0
-
+console.log(unitPrice);
 
 parentOfPrice.addEventListener("click" , function(event){
     if(event.target.id === "increase")
@@ -30,3 +34,31 @@ parentOfPrice.addEventListener("click" , function(event){
     }
 
 })
+
+function totalPrice (individualUnit, unitPrice) {
+    return checkout.innerHTML = `<div class=""checkout-div>
+    <div>
+        <img src="/images/image-product-1.jpg" class="product-image">
+    </div>
+    <div>
+        <p>Fall Limited Edition Sneakers</p>
+        <p>$ ${unitPrice} × ${individualUnit} $ ${unitPrice * individualUnit}</p>
+    </div>
+    <div>
+    </div>
+        <img src="/images/icon-delete.svg" alt="delete icon">
+    </div>`
+}
+
+button.addEventListener("click", function() {
+
+    if(individualUnit === 0)
+    {
+        alert("You have not added any item on the cart");
+    }
+
+    else {
+        totalPrice(individualUnit, unitPrice);
+    }
+    
+});
