cded52d30c81346a5378f103a8abdda1f63d91f4
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-12-16 04:30:28 +0100

    solve increase & decrease event handler

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 
 const parentOfPrice = document.querySelector(".price-container");
-console.log(parentOfPrice);
+const quantity = document.getElementById("quantity")
 
-parentOfPrice.
+let individualUnit = 0
+
+
+parentOfPrice.addEventListener("click" , function(event){
+    if(event.target.id === "increase")
+    {
+        individualUnit = individualUnit + 1;
+        sumOfUnit = individualUnit;
+        quantity.innerText = individualUnit;
+
+    }
+
+         if(event.target.id === "decrease")
+    {
+
+        individualUnit -= 1;
+        
+        if(individualUnit < 0) {
+            individualUnit = 0;
+             quantity.innerText = individualUnit;
+        }
+        else {
+            sumOfUnit = individualUnit;
+            quantity.innerText = individualUnit;
+        }
+    }
+
+})
